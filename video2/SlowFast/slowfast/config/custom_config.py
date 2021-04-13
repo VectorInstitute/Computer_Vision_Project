e2a32498dbc55acd54ebfbe500e214ff92b5fb36
Raghav Goyal <raghavgoyal14@gmail.com>
2021-04-12 23:46:10 -0400

    changes for mapping fully-connected layer

--- video2/SlowFast/slowfast/config/custom_config.py
+++ video2/SlowFast/slowfast/config/custom_config.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python3
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
 
+from fvcore.common.config import CfgNode
+
 """Add custom configs and default values"""
 
 
 def add_custom_config(_C):
     # Add your own customized configs.
-    pass
+    _C.TRAIN.CHECKPOINT_TRANSFER = CfgNode()
+
+    _C.TRAIN.CHECKPOINT_TRANSFER.ENABLE = False
+    _C.TRAIN.CHECKPOINT_TRANSFER.PATH_MAPPING = "/h/rgoyal/code/SlowFast/assets/class_mappings/dict_similarity_1.pkl"
+
